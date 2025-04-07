e6e8bb4c6b1e071cf744dcbab3a2eef91ad762a7
Revu <revubk123@gmail.com>
2025-04-07 23:37:43 +0530

    Added Error validation steps

--- tests/LoginValidations.test.js
+++ tests/LoginValidations.test.js
@@ -1,4 +1,4 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const testData = require('../resources/testData.json');
 const { loadLocatorOf } = require('../src/loadResources.js')
 const { launchApp } = require('../src/Driver.js')
@@ -21,12 +21,36 @@ test.describe('Login Validation', () => {
         await page.fill(await loadLocatorOf('PassWord'), data.password);
         await page.click(await loadLocatorOf('LoginButton'));
         
-        // if (data.expectedError) {
-        //   await expect(page.locator('.error-msg')).toHaveText(data.expectedError);
-        // } else {
-        //   await expect(page).toHaveURL('https://yourapp.com/dashboard');
-        // }
       });
     }
 });
 
+test.describe('Login Error Validations',  () => {
+
+    test(`Error Validations on input field`, async () => {
+
+      //empty fields
+      await page.click(await loadLocatorOf('LoginButton'));
+      await expect(page.locator(await loadLocatorOf('errorBelowUsername'))).toHaveText("Required");
+      await expect(page.locator(await loadLocatorOf('errorBelowPassword'))).toHaveText("Required");
+
+      //invalid username &password
+      await page.fill(await loadLocatorOf('UserName'), 'jj');
+      await expect(page.locator(await loadLocatorOf('errorBelowUsername'))).toHaveText("Email not valid");
+      await page.fill(await loadLocatorOf('PassWord'), 'jj');
+      await expect(page.locator(await loadLocatorOf('errorBelowPassword'))).toHaveText("Password too short");
+      await page.click(await loadLocatorOf('LoginButton'));
+
+
+      //clear input field
+      await page.fill(await loadLocatorOf('UserName'), '');
+      await page.fill(await loadLocatorOf('PassWord'), '');
+      await expect(page.locator(await loadLocatorOf('errorBelowUsername'))).toHaveText("Required");
+      await expect(page.locator(await loadLocatorOf('errorBelowPassword'))).toHaveText("Required");
+      await page.reload();
+      await expect(page.locator(await loadLocatorOf('errorBelowUsername'))).not.toBeVisible();
+      await expect(page.locator(await loadLocatorOf('errorBelowPassword'))).not.toBeVisible();
+      
+    });
+
+});
