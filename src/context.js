7dfc5298ec4f1eb368ffb8dadf4767daf97a2cfe
Revu <revubk123@gmail.com>
2025-04-27 23:11:31 +0530

    modified yml, modified jsons pom and some tests( not all)

--- src/context.js
+++ src/context.js
@@ -1,9 +1,12 @@
 import { launchApp, closeApp } from '../src/DriverControls'
 
 let _page;
+let _initialized = false;
 
 export async function getPage() {
-  if (!_page) {
+  if (!_page && _initialized==false) {
+    _initialized = true;
+    console.log('🚀 Creating NEW browser instance');
     _page = await launchApp();
   }
   return _page;
